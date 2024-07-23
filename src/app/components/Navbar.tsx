d1b80afd0592b2d3f0a49223f0ccd9d6f53de105
Hugh Culling <hughculling@icloud.com>
2024-07-23 10:47:27 +0100

    Completed Auth0 tutorial!

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,21 @@
+"use client";
+
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Navbar() {
+  const { user, error, isLoading } = useUser();
+  let status = "";
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    status = `Signed in as: ${user.name}`;
+  } else {
+    status = "User: not signed in";
+  }
+
   return (
     <>
       <Link href="/">Index</Link>
@@ -11,12 +26,12 @@ export default function Navbar() {
       <span> | </span>
       <Link href="/sign-up">Sign Up</Link>
       <span> | </span>
-      <Link href="/sign-in">Sign In</Link>
+      <a href="/api/auth/login">Sign In</a>
       <span> | </span>
       <Link href="/sign-in">User</Link>
       <span> | </span>
-      <Link href="/sign-out">Sign Out</Link>
-      <span className="status">User: not signed in</span>
+      <a href="/api/auth/logout">Sign Out</a>
+      <span className="status">{status}</span>
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export const metadata = {
   title: "Live Stream Music",
@@ -13,18 +14,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-sans">
-        <h1>Live Stream Music</h1>
-        <hr />
-        <Navbar />
-        <hr />
-        {children}
-        <hr />
-        <p>Author: Hugh Wilfred Culling</p>
-        <p>
-          <a href="mailto:hughculling@icloud.com">hughculling@icloud.com</a>{" "}
-        </p>
-      </body>
+      <UserProvider>
+        <body className="font-sans">
+          <h1>Live Stream Music</h1>
+          <hr />
+          <Navbar />
+          <hr />
+          {children}
+          <hr />
+          <p>Author: Hugh Wilfred Culling</p>
+          <p>
+            <a href="mailto:hughculling@icloud.com">hughculling@icloud.com</a>{" "}
+          </p>
+        </body>
+      </UserProvider>
     </html>
   );
 }
