8342c39c6e500bb5d9f76b7b62d7f0e1c3d50ac2
Hugh Culling <hughculling@icloud.com>
2024-07-27 17:50:42 +0100

    Got playback working with script tag.

--- src/app/playback/page.tsx
+++ src/app/playback/page.tsx
@@ -1,12 +1,29 @@
-export const metadata = {
-  title: "Playback | Live Stream Music",
-};
+"use client";
+
+import Script from "next/script";
 
 export default function PlaybackPage() {
   return (
     <>
       <h2>Playback</h2>
       <video id="video-player" playsInline></video>
+      <Script
+        src="https://player.live-video.net/1.30.0/amazon-ivs-player.min.js"
+        onLoad={() => {
+          const IVSPlayer = (window as any).IVSPlayer;
+
+          if (IVSPlayer.isPlayerSupported) {
+            const player = IVSPlayer.create();
+            player.attachHTMLVideoElement(
+              document.getElementById("video-player")
+            );
+            player.load(
+              "https://548ea801f896.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.117906616901.channel.eAG7DyR7da0W.m3u8"
+            );
+            player.play();
+          }
+        }}
+      ></Script>
     </>
   );
 }
