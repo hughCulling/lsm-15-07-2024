b82daa74dcff40934efc71bf40f46737413ea9ee
Hugh Culling <hughculling@icloud.com>
2024-08-05 21:44:33 +0100

    Got production tokens working.

---
+++ src/app/api/get-token/route.js
@@ -0,0 +1,29 @@
+// app/api/get-token/route.js
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function POST() {
+  const options = {
+    method: "POST",
+    url: "https://dev-acqqi6nb00ynyme4.us.auth0.com/oauth/token",
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    data: new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: "9D916kiBDyY2ZTi9Wo3VRvGO6hvttKql",
+      client_secret:
+        "0oRelwLcECBnbqcIIgeasJC48gi2Zht-wGcdMJCNnCyDBQDvVI72sinP4k5k4Df-",
+      audience: "https://dev-acqqi6nb00ynyme4.us.auth0.com/api/v2/",
+    }),
+  };
+
+  try {
+    const response = await axios.request(options);
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
