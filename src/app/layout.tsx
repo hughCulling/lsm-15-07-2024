8e48c1883b690d046bad233ec43a3ba332ba6143
Hugh Culling <hughculling@icloud.com>
2024-08-07 20:10:59 +0100

    Small change to cause deployment.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           <hr />
           {children}
           <hr />
-          <p>Author: Hugh Wilfre Culling</p>
+          <p>Author: Hugh Wilfred Culling</p>
           <p>
             <a href="mailto:hughculling@icloud.com">hughculling@icloud.com</a>{" "}
           </p>
